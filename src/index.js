a4acafa6f4dd45abfb29add35ff2fc7092e84101
JDS555 <nfive.5.101@gmail.com>
2024-11-24 17:31:41 +0300

    feat: practice 2024-11-24

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-module.exports = function reverse (number) {
-
-    let reversed = 0;
-    let numberLength = Math.floor(Math.log10(Math.abs(number))) + 1;
-
-    for (let i = 0; i < numberLength; i++) {
-        reversed = reversed * 10 + (Math.floor(Math.abs(number) / Math.pow(10, i))) % 10;
-    }
-
-    return reversed;
-    
+module.exports = function reverse(n) {
+    const absoluteValue = Math.abs(n);
+    const reversedNumber = Number(absoluteValue.toString().split('').reverse().join(''));
+    return reversedNumber;
 }
